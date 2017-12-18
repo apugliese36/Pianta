626bdf822a49dcb9923a9cb0aa2029a64ffe0ebd
Antonio Pugliese <apugliese74@gmail.com>
2017-12-18 14:19:01 -0500

    added garden model tests

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Garden, type: :model do
   it { should have_valid(:name).when('Cute Plantyz') }
   it { should_not have_valid(:name).when(nil, '') }
 
-  it { should have_valid(:description).when('My balcony garden! Facing east.', nil, '') }
+  it { should have_valid(:description).when('My balcony garden!', nil, '') }
 
   it { should have_valid(:photo).when('garden_pic.png', nil, '') }
 
-  it { should have_valid(:user_id).when(4, 1937343) }
+  it { should have_valid(:user_id).when(4, 1_937_343) }
   it { should_not have_valid(:user_id).when(nil, '') }
 end
