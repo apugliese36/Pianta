6efef83f1ad85009a4baaf64a6937f65254b3016
Antonio Pugliese <apugliese74@gmail.com>
2017-12-19 12:10:45 -0500

    added faux sign in button to go to react

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root "static_pages#index"
+  root "sign_in#index"
 
   namespace :api do
     namespace :v1 do
       resources :gardens, only: [:index, :show, :create, :destroy]
     end
   end
-  
+
+  resources :static_pages, only: [:index]
+
 end
