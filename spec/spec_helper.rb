695acabe030d1d6bdfcf42cec2723d34e899f9ab
Antonio Pugliese <apugliese74@gmail.com>
2017-12-19 10:38:35 -0500

    added model level tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,8 @@ SimpleCov.start do
   add_filter 'app/mailers'
   add_filter 'app/jobs'
   add_filter 'app/helpers'
+  add_filter 'app/controllers'
+  add_filter 'app/serializers'
 end
 
 RSpec.configure do |config|
