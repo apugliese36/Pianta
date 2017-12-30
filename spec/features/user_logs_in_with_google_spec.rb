09e2122193ce584d8b1365860d181819357638fe
Antonio Pugliese <apugliese74@gmail.com>
2017-12-30 14:19:22 -0500

    styled sign in page and fixed test accordingly

--- spec/features/user_logs_in_with_google_spec.rb
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -5,8 +5,8 @@ RSpec.feature 'user logs in' do
   scenario 'using google oauth2' do
     stub_omniauth
     visit root_path
-    expect(page).to have_link('Sign in with Google')
-    click_link 'Sign in with Google'
-    expect(page).to_not have_link('Sign in with Google')
+    expect(page).to have_content('Welcome to')
+    click_link 'google-sign-in'
+    expect(page).to_not have_link('Welcome to')
   end
 end
